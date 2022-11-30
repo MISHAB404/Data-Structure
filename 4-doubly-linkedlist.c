847fb06c48f57617c87645dfafe55ec5f838f369
Mishab <113359139+mishab404@users.noreply.github.com>
2022-11-30 12:08:30 +0530

    Update 4-doubly-linkedlist.c

--- 4-doubly-linkedlist.c
+++ 4-doubly-linkedlist.c
@@ -7,51 +7,51 @@ struct node
   int data;
   struct node *left,*right;
  };
-
-struct node *start=NULL;
-void main()
+struct node * start=NULL;
+void main() 
  {
   struct node* search(int);
-  void delete(int);
+  void delete(int); 
   void insert(int);
   int data,option;
   struct node *t;
-  printf("...................DOUBLY LINKED LIST................\n");
+  printf("-----------------------DOUBLY LINKED LIST-----------------------\n");
    do
     {
-     printf("\n 1.Insert \n 2.Search \n 3.Delete \n 4.Exit");
-     printf("---Enter your option : \n");
+     printf("\n 1 --- Insert \n 2 --- Search \n 3 --- Delete \n 4 --- Exit \n");
+     printf("\n Enter your option : ");
      scanf("%d",&option);
-      switch(option)
-        {
-         case 1:printf("Enter the data\n");
-                scanf("%d",&data);
-		insert(data);
-		break;
+   switch(option)
+    {
+     case 1:printf("\n - Enter the data : ");
+            scanf("%d",&data);
+            insert(data);
+            break;
 
-         case 2:printf("enter the data to search\n");
-		scanf("%d",&data);
-		t=search(data);
-		 if(t==(struct node*)0)
-		   printf("ITEM NOT FOUND:....\n\n");
-		 else
-		  printf("ITEM FOUND\n\n");
-	        break;
+     case 2:printf("\n - Enter the data to search : ");
+            scanf("%d",&data);
+            t=search(data);
+             if(t==(struct node*)0)
+               printf("\n ( !!!ITEM NOT FOUND!!! )\n");
+             else
+               printf("\n ( ITEM FOUND )\n");
+            break;
 
-         case 3:printf("Enter the item to delete\n");
-		scanf("%d",&data);
-		delete(data);
-		printf("Deleted item is:%d",data);
-		printf("\n");
-		break;
+     case 3:printf("\n - Enter the item to delete : ");
+            scanf("%d",&data);
+            delete(data);
+            printf("\n ( %d is Deleted )",data);
+            printf("\n");
+            break;
 
-	 case 4:exit(1);
-       }// this end of switch
+     case 4:exit(1);
+    } // this end of switch
    }
-   while(1);
+while(1);
  }
 
-//function to insert node at begining of
+//function to insertion
+
 void insert(int data)
  {
   struct node *t;
@@ -60,47 +60,57 @@ void insert(int data)
    if(start==(struct node*)0)
      t->left=t->right=(struct node*)0;
    else
+    {
      t->left=(struct node*)0;
      t->right=start;
      start->left=t;
+    }
+  start=t;
  }
- start=t;
 
-// function for search
+//function to Search
+
 struct node* search(int data)
  {
   struct node *t=start;
-  while(t!=(struct node*)0 && t->data!=data)
-   {
-    t=t->right;
-   }
+   while(t!=(struct node*)0 && t->data!=data)
+    {
+     t=t->right;
+    }
   return t;
  }
 
-// function for delete
+//function to Deletion
+
 void delete(int data)
  {
   struct node *t;
   t=search(data);
    if(t==0)
-    printf("NOT FOUND DATA\n\n");
+    printf("\n ( !!!NOT FOUND DATA!!! )\n");
    else
+    {
      if(t==start) //case of first node
-      if(t->right==(struct node*)0)
-       start=(struct node*)0;//one and only one node
+      {
+        if(t->right==(struct node*)0)
+        start=(struct node*)0; //one and only one node
      else
        {
-        t->right->left=t->left;//remove 1st node ,more than one node
+        t->right->left=t->left; //remove 1st node ,more than one node
         start=t->right;
        }
-
-   else if(t->right==(struct node*)0) // case of last node
-          t->left->right=t->right;
-   else //case of inter node
-      {
-	t->left->right=t->right;
-	t->right->left=t->left;
       }
+
+     else if(t->right==(struct node*)0) // case of last node
+       {
+        t->left->right=t->right;
+       }
+   else//case of inter node
+    {
+     t->left->right=t->right;
+     t->right->left=t->left;
+    }
 free(t);
-}
+   }
+ }
 
